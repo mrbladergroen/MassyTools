eea530428503483983f4dda3e2a34bb0bac69a34
Bladergroen <m.r.bladergroen@lumc.nl>
2023-12-15 12:03:42 +0100

    Command prompt starts minimized

--- MassyTools.bat
+++ MassyTools.bat
@@ -1,5 +1,6 @@
 echo off
-
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 REM Set dir to where this batch file is started from
-cd /D "%~dp0"
+cd /d "%~dp0"
 python MassyTools.py
+exit
